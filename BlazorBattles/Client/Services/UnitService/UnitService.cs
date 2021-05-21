236e9e80ab8e95d2f2d13c5e6c5343f560a2f66d
Andreia Correia <amrcorreia@users.noreply.github.com>
2021-05-21 17:23:46 +0100

    Select & foreach in Action (Extending the Build Component)

--- BlazorBattles/Client/Services/UnitService/UnitService.cs
+++ BlazorBattles/Client/Services/UnitService/UnitService.cs
@@ -20,6 +20,8 @@ namespace BlazorBattles.Client.Services.UnitService
         {
             Unit unit = Units.First(unit => unit.Id == unitId);
             MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints});
+            Console.WriteLine($"{unit.Title} was built!");
+            Console.WriteLine($"Your army size: {MyUnits.Count}.");
         }
     }
 }
