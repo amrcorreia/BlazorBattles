8acb6a1bbe8d3f1c6f55d76945e829a217d8428c
Andreia Correia <amrcorreia@users.noreply.github.com>
2021-05-21 16:50:58 +0100

    Add Apples with the ApplesService

---
+++ BlazorBattles/Client/Services/ApplesService/ApplesService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services.ApplesService
+{
+    public class ApplesService : IApplesService
+    {
+        public int Apples { get; set; } = 7;
+
+        public event Action OnChange;
+
+        public void AddApples(int amount)
+        {
+            Apples += amount;
+            ApplesChanged();
+        }
+
+        public void EatApples(int amount)
+        {
+            Apples -= amount;
+            ApplesChanged();
+        }
+
+        void ApplesChanged() => OnChange.Invoke();
+    }
+}
---
+++ BlazorBattles/Client/Services/ApplesService/IApplesService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Client.Services.ApplesService
+{
+    public interface IApplesService
+    {
+        event Action OnChange;
+
+        int Apples { get; set; }
+
+        void EatApples(int amount);
+
+        void AddApples(int amount);
+    }
+}
