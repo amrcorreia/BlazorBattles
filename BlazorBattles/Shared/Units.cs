7128c8302037cb2f5f48e3af1c62813808ac7537
Andreia Correia <amrcorreia@users.noreply.github.com>
2021-05-21 16:56:38 +0100

    Add Unit & UserUnit Classes

---
+++ BlazorBattles/Shared/Units.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBattles.Shared
+{
+    public class Units
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Attack { get; set; }
+
+        public int Defence { get; set; }
+
+        public int HitPoints { get; set; }
+
+        public int BananaCost { get; set; }
+    }
+}
