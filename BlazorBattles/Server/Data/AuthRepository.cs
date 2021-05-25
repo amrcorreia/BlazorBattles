15ff73ff2df3289d7a68320db069f355a53ca8f3
Andreia Correia <amrcorreia@users.noreply.github.com>
2021-05-25 15:38:49 +0100

    User Registration Service Response with Generics

--- BlazorBattles/Server/Data/AuthRepository.cs
+++ BlazorBattles/Server/Data/AuthRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorBattles.Shared;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,19 +9,58 @@ namespace BlazorBattles.Server.Data
 {
     public class AuthRepository : IAuthRepository
     {
-        public Task<string> Login(string email, string password)
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ServiceResponse<string>> Login(string email, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<int> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<int> { 
+                    Success = false,
+                    Message = "User already exists"
+                };
+            }
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int>
+            {
+                Data = user.Id,
+                Message = "Registration sucessful!"
+            };
         }
 
-        public Task<bool> UserExists(string email)
+        public async Task<bool> UserExists(string email)
         {
-            throw new NotImplementedException();
+            if (await _context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
